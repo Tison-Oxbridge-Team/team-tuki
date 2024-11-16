118d754e6944b847d72c26a41239b2da2a289638
Vaniah Ndungu <ndunguvaniah@gmail.com>
2024-11-17 02:40:01 +0300

    rounds schema updated

--- api/models/Round.js
+++ api/models/Round.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const RoundSchema = new mongoose.Schema({
     id: { type: String, required: true },
     name: { type: String, required: true },
-    stage: { type: String, enum: ['initial', 'semifinal', 'final'], required: true },
+    stage: { type: String, enum: ['screening', 'semifinal', 'final'], required: true },
     startTime: { type: Date, required: true },
     endTime: { type: Date, required: true },
     createdAt: { type: Date, default: Date.now },
